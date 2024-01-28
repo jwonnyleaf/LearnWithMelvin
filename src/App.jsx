dba9008bc4fa1f8ff2cf2a974f67c12dc4317e4f
MARCI VAN <mvan122004@gmail.com>
2024-01-28 07:49:54 -0600

    task1

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
 import About from "./components/About/About";
 import Dashboard from "./components/Dashboard/Dashboard";
+import PartOne from "./components/Challenge1/PartOne";
+import PartTwo from "./components/Challenge1/PartTwo";
+import PartThree from"./components/Challenge1/PartThree";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 const theme = createTheme({
@@ -124,6 +127,9 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/taskonepartone" element={<PartOne />} />
+            <Route path="/taskoneparttwo" element={<PartTwo />} />
+            <Route path="/taskonepartthree" element={<PartThree />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </AuthProvider>
---
+++ src/components/Challenge1/PartOne.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { useUserAuth } from "../../contexts/AuthContext";
+import { Logo, FNAFBear } from "../../assets";
+
+import "./PartOne.css"
+
+const PartOne = () => {
+    const { signOut } = useUserAuth();
+    return (
+        <>
+            <div>
+                <h1>Task 1: Help Melvin Escape the Animatronic ᕦʕ •ᴥ•ʔᕤ</h1>
+            </div>
+            <p class="text-left">
+                <h3>Howdy! It’s seems like you’re the only one that can hear me.. <br></br>
+                    You need to help me escape this place! I am currently stuck in this <br></br>
+                    pizza parlor and I keep hearing these footsteps outside of this room. I need <br></br>
+                    to survive this place to make it back home</h3>
+                <h5>Please help Melvin avoid and disable the animatronic to survive the night! Enter a room to explore the facility however beware! There may be a foe behind the door.</h5>
+                <hr></hr>
+            </p>
+            <img src={Logo} className='partonelogo' rounded></img>
+            <Button variant="contained" color="ashgreen" ><h2>ROOM ONE</h2></Button>
+            <span> </span>
+            <Button variant="contained" color="ashgreen" href="./taskoneparttwo"><h2>ROOM TWO</h2></Button>
+            <span> </span>
+            <Button variant="contained" color="ashgreen" ><h2>ROOM THREE</h2></Button>
+            <img src={FNAFBear} className='partonefnaf' rounded></img>
+        </>
+
+    );
+};
+
+export default PartOne;
